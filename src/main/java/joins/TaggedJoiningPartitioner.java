1ea6aad48580ffa430442ae47ca34d4ab12aa35f
Kostas Perifanos <kostas.perifanos@gmail.com>
2016-04-05 23:45:44 +0100

    some more

---
+++ src/main/java/joins/TaggedJoiningPartitioner.java
@@ -0,0 +1,16 @@
+package joins;
+
+/**
+ * Created by kostas on 05/04/16.
+ */
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class TaggedJoiningPartitioner extends Partitioner<TaggedKey,Text> {
+
+    @Override
+    public int getPartition(TaggedKey taggedKey, Text text, int numPartitions) {
+        return taggedKey.getJoinKey().hashCode() % numPartitions;
+    }
+}
